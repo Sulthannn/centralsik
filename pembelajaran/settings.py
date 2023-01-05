76faf76a4166a7957c6ffb1342f6f7a3df407719
unknown <Sulthannn>
2023-01-06 01:39:46 +0700

    commit

--- pembelajaran/settings.py
+++ pembelajaran/settings.py
@@ -131,6 +131,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'central'
 
